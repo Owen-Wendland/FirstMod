16925811618b65b7133ecf5dd83db6f2fd1a29db
Owen_Wendland <owsun21@gmail.com>
2024-11-26 20:26:29 -0800

    Made jhon poisonous and edible

--- src/main/java/net/john/firstmod/Item/ModItems.java
+++ src/main/java/net/john/firstmod/Item/ModItems.java
@@ -1,6 +1,9 @@
 package net.john.firstmod.Item;
 
 import net.john.firstmod.FirstMod;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,7 +14,12 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
 
-    public static final RegistryObject<Item> JHON = ITEMS.register("jhon", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> JHON = ITEMS.register("jhon",
+            () -> new Item(new Item.Properties()
+                    .food(new FoodProperties.Builder().nutrition(100).saturationModifier(1000)
+                            .effect(new MobEffectInstance(MobEffects.LEVITATION,200,0), 1f).alwaysEdible()
+                            .effect(new MobEffectInstance(MobEffects.POISON,100,0), 1f)
+                            .build())));
     public static final RegistryObject<Item> GOKU = ITEMS.register("goku", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
